ca06abb4259bfacbb353d108295aa8b3d3706e1d
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-01-21 20:25:16 -0500

    added deposit class

--- index.js
+++ index.js
@@ -12,7 +12,15 @@ class Withdrawal {
 
 }
 
+class Deposit {
+  constructor(amount) {
+    this.amount = amount;
+  }
 
+  commit() {
+    balance += this.amount
+  }
+}
 
 
 // DRIVER CODE BELOW
@@ -27,3 +35,6 @@ t2.commit();
 console.log('Transaction 2:', t2);
 
 console.log('Balance:', balance);
+t3 = new Deposit(120.00);
+t3.commit();
+console.log("Transaction 3:",t3);
